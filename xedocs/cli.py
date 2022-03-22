0a6277b09732b0cb2484239197fdf5a124d17a57
Yossi Mosbacher <joe.mosbacher>
2022-03-23 00:51:40 +0200

    error handling for cli

--- xedocs/cli.py
+++ xedocs/cli.py
@@ -1,12 +1,22 @@
 """Console script for xedocs."""
 import sys
-
+import xedocs
 import click
+import logging
+
+from rich.logging import RichHandler
 from rich.console import Console
 from rich.table import Table
 
-table = Table(title="Star Wars Movies")
-import xedocs
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
+
+logger = logging.getLogger("xedocs")
+
 
 @click.group()
 def main():
@@ -14,7 +24,7 @@ def main():
     click.echo("Welcome to the xedocs CLI")
     
     return 0
-    
+
 
 @main.command(name='find', context_settings=dict(
     ignore_unknown_options=True,
@@ -23,12 +33,18 @@ def main():
 @click.argument('name')
 @click.pass_context
 def cli_find(ctx, name: str):
+    console = Console()
     kwargs = dict([item.strip('--').split('=') for item in ctx.args])
     kwargs = {k: v.split(',') if ',' in v else v for k,v in kwargs.items()}
-    schema = xedocs.find_schema(name)
-
-    click.echo(f"Looking for {name} documents that match your query...")
-    docs = schema.find(**kwargs)
+    try:
+        schema = xedocs.find_schema(name)
+    except KeyError:
+        logger.error(f'Cant find a schema for `{name}`\n'
+                    f'Available schemas: {xedocs.list_schemas()}'
+        )
+        exit(0)
+    with console.status(f"[bold green]Looking for {name} documents that match your query...") as status:
+        docs = xedocs.find(name, **kwargs)
     query_str = ", ".join([f'{k}={v}' for k,v in kwargs.items()])
 
     table = Table(title=f"{name.title()} query {query_str}")
@@ -47,7 +63,6 @@ def cli_find(ctx, name: str):
         row = [str(data[field]) for field in field_order]
         table.add_row(*row)
 
-    console = Console()
     console.print(table)
 
 if __name__ == "__main__":
