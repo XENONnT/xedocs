a875af374927503788df97f367b4f2dbc9ec5796
Yossi Mosbacher <joe.mosbacher>
2022-04-14 23:19:07 +0300

    Bump version: 0.1.2 → 0.1.3

--- xedocs/__init__.py
+++ xedocs/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Yossi Mosbacher"""
 __email__ = "joe.mosbacher@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from ._settings import settings
 from ._frames import frames
