84b2687f90ce26a39b993137a1d4dfa0a0a5e579
Yossi Mosbacher <joe.mosbacher>
2022-07-29 10:39:56 +0300

    add anode washing schema

--- xedocs/schemas/operations_reports/__init__.py
+++ xedocs/schemas/operations_reports/__init__.py
@@ -1,3 +1,4 @@
 from .warmspot import WarmspotReport
 from .hotspot import HotspotReport
 from .anode_ramp import AnodeRampReport
+from .anode_washing import AnodeWashingReport
---
+++ xedocs/schemas/operations_reports/anode_washing.py
@@ -0,0 +1,18 @@
+import pydantic
+import datetime
+from typing import Literal, List
+from .base_report import BaseOperationsReport
+
+
+class LevelChange(pydantic.BaseModel):
+    time: datetime.datetime
+    liquid_level: float = pydantic.Field(lt=8, ge=0)
+
+
+class AnodeWashingReport(BaseOperationsReport):
+    """Anode washing report
+    """
+
+    _ALIAS = "anode_washes"
+    
+    washes: List[LevelChange]
