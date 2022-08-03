d8f078ad9084504a9f96f0b18296d27ca7622e95
Yossi Mosbacher <joe.mosbacher>
2022-08-03 16:59:45 +0300

    add abnormal rates scherma

--- xedocs/schemas/operations_reports/__init__.py
+++ xedocs/schemas/operations_reports/__init__.py
@@ -1,3 +1,4 @@
 from .hotspot import HotspotReport
 from .anode_ramp import AnodeRampReport
 from .anode_washing import AnodeWashingReport
+from .abnormal_rates import AbnormalDAQRate
---
+++ xedocs/schemas/operations_reports/abnormal_rates.py
@@ -0,0 +1,18 @@
+
+from .base_report import BaseOperationsReport
+
+
+class AbnormalDAQRate(BaseOperationsReport):
+    """Abnormal DAQ rate report"""
+
+    _ALIAS = "abnormal_daq_rates"
+
+    anode_voltage_kv: float
+    action_taken: str
+    plot: str
+
+    reader0_rate: float
+    reader1_rate: float
+    reader2_rate: float
+    muon_veto_rate: float
+    nveto_rate: float
