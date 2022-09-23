f108023a1fc5b97e70538e4ad930f4f45b7e8785
Luis Sanchez <luisanchez25o@gmail.com>
2022-09-23 16:15:53 -0500

    Add relative light yield schema

---
+++ xedocs/schemas/corrections/relative_light_yield.py
@@ -0,0 +1,20 @@
+'''
+# Correction: Relative light yield
+# What is it correction: Corrected areas (cS1)
+
+# Description: Correction done to the area of S1s in our detector. The relative light yield corresponds to the number of photons in the visible light spectrum released by an interaction per energy. This correction helps us convert from the number of photons observed in an S1 event to an energy.
+
+# Latest wiki reference: 
+
+'''
+
+
+import rframe
+
+from .base_corrections import TimeSampledCorrection
+
+class RelativeLightYield(xd.schemas.base_corrections.TimeSampledCorrection):
+    
+    _ALIAS = "relative_light_yield"
+    value: float #we can spesify that type of data expected in each field to make sure all inputs are correcrt
+    version: str = rframe.Index()
