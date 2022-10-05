ab4d1d4963d5b5e3668b3d3b901e1f4e2b40849e
Yossi Mosbacher <joe.mosbacher>
2022-10-05 21:47:04 +0300

    adapt global versions for urlconfigs

--- xedocs/schemas/global_versions.py
+++ xedocs/schemas/global_versions.py
@@ -6,21 +6,7 @@ from .base_schemas import VersionedXeDoc
 class GlobalVersion(VersionedXeDoc):
     _ALIAS = "global_versions"
 
-    strax_version: str = rframe.Index()
-    straxen_version: str = rframe.Index()
+    config_name: str = rframe.Index(max_length=80)
+    correction: str = rframe.Index(max_length=80)
 
-    @classmethod
-    def get_global_config(cls, version, datasource=None, names=None, extra_labels=None):
-        """Build a context config from the given global version."""
-        import straxen
-
-        if extra_labels is None:
-            extra_labels = dict(run_id="plugin.run_id")
-        refs = cls.find(datasource, version=version, alias=names)
-        config = {}
-        for ref in refs:
-            url = ref.url_config
-            if extra_labels is not None:
-                url = straxen.URLConfig.format_url_kwargs(url, **extra_labels)
-            config[ref.alias] = url
-        return config
+    url: str
