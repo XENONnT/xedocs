abe1a67807af68c1827d8c630f537ff29a941290
Luis Sanchez <45677170+LuisSanchez25@users.noreply.github.com>
2022-11-13 19:13:29 -0600

    Update bayes_model.py

--- xedocs/schemas/corrections/bayes_model.py
+++ xedocs/schemas/corrections/bayes_model.py
@@ -1,9 +1,14 @@
 """
 
 """
+import datetime
+from typing import Literal
+import rframe
 
-from .base_corrections import TimeIntervalCorrection
+from .base_references import BaseResourceReference
 
-
-class NaiveBayesClassifier(TimeIntervalCorrection):
+class NaiveBayesClassifier(BaseResourceReference):
     _ALIAS = "bayes_models"
+    
+    time: rframe.Interval[datetime.datetime] = rframe.IntervalIndex()
+    value: str
