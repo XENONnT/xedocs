d8e361e1e0fb84b14c1421fa826e11528d815c65
Luis Sanchez <45677170+LuisSanchez25@users.noreply.github.com>
2022-12-09 17:27:42 -0600

    Update test_single_reprocess_cutax.py

--- tests/test_single_reprocess_cutax.py
+++ tests/test_single_reprocess_cutax.py
@@ -1,4 +1,4 @@
-from .functions_for_xedocs_test import save_test_data, check_reprocessing_cutax
+from .functions_for_xedocs_test * # import save_test_data, check_reprocessing_cutax
 from .variables_for_test import *
 import xedocs
 import straxen
