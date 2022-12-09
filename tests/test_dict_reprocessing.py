2ed547e8b2386d57bb252721be0f9f2e07c40c20
Luis Sanchez <45677170+LuisSanchez25@users.noreply.github.com>
2022-12-09 16:26:44 -0600

    Update test_dict_reprocessing.py

--- tests/test_dict_reprocessing.py
+++ tests/test_dict_reprocessing.py
@@ -1,5 +1,5 @@
-from functions_for_xedocs_test import save_test_data, check_reprocessing_dict
-from variables_for_test import *
+from .functions_for_xedocs_test import save_test_data, check_reprocessing_dict
+from .variables_for_test import *
 import pytest
 
 
