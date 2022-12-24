c8bf8a96b7877f9f602a7729fba883940d879481
Yossi Mosbacher <joe.mosbacher>
2022-12-24 20:46:39 +0200

    import new schemas

--- xedocs/schemas/__init__.py
+++ xedocs/schemas/__init__.py
@@ -1,5 +1,7 @@
 from .base_schemas import VersionedXeDoc, XeDoc
 from .detector_numbers import DetectorNumber
+from .context_config import ContextConfig
+from .context_lineage import ContextLineage
 from .fax import FaxConfig
 from .corrections import *
 from .calibrations import *
