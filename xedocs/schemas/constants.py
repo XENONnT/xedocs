c5f6bd95672f6583a715d9df7291210b9eb9cbf5
Yossi Mosbacher <joe.mosbacher@gmail.com>
2023-03-23 18:02:00 +0200

    add rn-222 as a source option

--- xedocs/schemas/constants.py
+++ xedocs/schemas/constants.py
@@ -6,6 +6,7 @@ SOURCE = Literal[
     "ar-37",
     "kr-83m",
     "rn-220",
+    "rn-222",
     "led",
     "pulser",
     "noise",
@@ -16,7 +17,7 @@ SOURCE = Literal[
     "YBe",
 ]
 
-DIFFUSED_SOURCE = Literal["rn-220", "kr-83m", "ar-37"]
+DIFFUSED_SOURCE = Literal["rn-220", "rn-222", "kr-83m", "ar-37",]
 
 DETECTOR = Literal["tpc", "neutron_veto", "muon_veto", "tpc_he"]
 
