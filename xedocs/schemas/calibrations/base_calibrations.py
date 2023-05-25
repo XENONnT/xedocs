a1a609835a3dbab50d84f227dd4de7db2185fec3
Yossi Mosbacher <joe.mosbacher@gmail.com>
2023-05-25 17:31:17 +0300

    replace time alias with root validators

--- xedocs/schemas/calibrations/base_calibrations.py
+++ xedocs/schemas/calibrations/base_calibrations.py
@@ -1,8 +1,10 @@
 import datetime
 import pydantic
 import rframe
-from pydantic import validator
-from typing import List, Literal
+import pandas as pd
+from pydantic import root_validator, validator
+from typing import List
+from rframe.types import TimeInterval
 
 from ..base_schemas import XeDoc
 from ..._settings import settings
@@ -19,7 +21,7 @@ class BaseCalibration(XeDoc):
     class Config:
         allow_population_by_field_name = True
 
-    time: rframe.Interval[datetime.datetime] = rframe.IntervalIndex(alias="run_id")
+    time: rframe.Interval[datetime.datetime] = rframe.IntervalIndex()
     source_id: str = rframe.Index(min_length=1, max_length=60)
 
     source_type: SOURCE
@@ -29,12 +31,28 @@ class BaseCalibration(XeDoc):
     run_ids: List[str] = []
     filled_by: str = pydantic.Field(min_length=1, max_length=60, default="")
 
+
     @validator("time", pre=True)
-    def run_id_to_time(cls, v):
-        """Convert run id to time"""
-        if isinstance(v, (str, int)):
+    def time_string_to_interval(cls, v):
+        """Convert str to time interval"""
+        if isinstance(v, str):
             try:
-                v = settings.run_id_to_interval(v)
+                if "," in v:
+                    left, right = v.split(",")
+                    left, right = pd.to_datetime(left), pd.to_datetime(right)
+                    v = TimeInterval(left=left, right=right)
+                else:
+                    v = pd.to_datetime(v)
             except:
                 pass
         return v
+
+    @root_validator(pre=True)
+    def run_id_to_time_interval(cls, values):
+        if "run_id" in values:
+            run_id = values.pop("run_id")
+            try:
+                values["time"] = settings.run_id_to_interval(run_id)
+            except:
+                values["time"] = run_id
+        return values
