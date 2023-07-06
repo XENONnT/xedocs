feed27c8d1c768a18b16f293164fe9862201a792
Luis Sanchez <luisanchez25o@gmail.com>
2023-07-06 13:52:33 -0500

    make SOM schema

---
+++ xedocs/schemas/corrections/som_network.py
@@ -0,0 +1,15 @@
+"""
+Algorithm which uses a pretrained weightcube from the output of a 
+Self-Organizing Map to classify peaklets as one of several SOM_types,
+as well as classifying those types into either an S1 or an S2
+"""
+
+from .base_references import BaseResourceReference
+
+
+class SOMClassifier(BaseResourceReference):
+    _Alias = 'som_classification'
+    fmt = 'binary'
+    
+    value: str
+    
