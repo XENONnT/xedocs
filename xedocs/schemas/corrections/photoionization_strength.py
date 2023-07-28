ab6c7ff6d351574030835243835ffc1596cd0a61
Shenyang Shi <ss6109@columbia.edu>
2023-07-28 08:35:57 -0500

    Add photoionizations

---
+++ xedocs/schemas/corrections/photoionization_strength.py
@@ -0,0 +1,11 @@
+"""
+# Photoionization strengths
+https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenonnt_sr1:photoionization_origin
+"""
+
+from .base_corrections import TimeSampledCorrection
+
+
+class PhotoionizationStrength(TimeSampledCorrection):
+    _ALIAS = "photoionization_strength"
+    value: float
