bb52352695824170a345c252af07d37a541f80a3
Zihao Xu <zihao.xu@columbia.edu>
2023-08-31 08:30:55 -0500

    rename

--- xedocs/schemas/corrections/cs2_aft_corrections.py
+++ xedocs/schemas/corrections/cs2_aft_corrections.py
@@ -1,5 +1,5 @@
 """
-# cS2 AFT
+# cS2 bottom top ratio correction
 Due to photoionizations, the cS2_bottom has a time-dependent charge yield.
 
 https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenonnt:zihao:sr1_s2aft_photonionization_correction
@@ -9,7 +9,7 @@ https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenonnt:zihao:sr1_s2aft_photoni
 from .base_corrections import TimeSampledCorrection
 
 
-class cS2AFTCorrection(TimeSampledCorrection):
-    _ALIAS = "cs2_aft_corrections"
+class cS2BottomTopRatioCorrection(TimeSampledCorrection):
+    _ALIAS = "cs2_bottom_top_ratio_correction"
     value: float
     
