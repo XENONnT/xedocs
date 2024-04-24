6d5a45eac0af76b869dcab78cf486106d333e6b3
Giovanni Volta <gvolta@midway3-login4.rcc.local>
2024-04-24 11:44:59 -0500

    Merg AB and CD partitioning into avg SEG schema

--- xedocs/schemas/corrections/implementations/avg_se_gain.py
+++ xedocs/schemas/corrections/implementations/avg_se_gain.py
@@ -1,10 +1,19 @@
 """
+This schema incorporates two iece of inforamtion for the SEG & EE correciont. Namely the average single electron gain
+and the definition of the AB and CD partitions
+
 Correction: Average single electron gain
 Affects: Corrected areas
 
 Average of the single electron corrections. This correction my also have two partitions as this average is a temporal average not a spatial one.
 
-wiki:
+Correction: Region linear and circular for AB/CD partitions
+Affects: Corrected areas
+
+Two distinct patterns of evolution of single electron corrections between A+B and C+D. Distinguish thanks to linear and circular regions
+SR0 wiki: https://xe1t-wiki.lngs.infn.it/doku.php?id=jlong:sr0_2_region_se_correction
+SR1 wiki: https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenonnt:noahhood:corrections:se_gain_ee_final
+
 """
 
 import rframe
@@ -16,5 +25,7 @@ from ...constants import PARTITION
 class AvgSingleElectronGain(TimeIntervalCorrection):
 
     _ALIAS = "avg_se_gains"
+    field: str = rframe.Index(max_length=80)
     partition: PARTITION = rframe.Index(default="all_tpc")
+    
     value: float
