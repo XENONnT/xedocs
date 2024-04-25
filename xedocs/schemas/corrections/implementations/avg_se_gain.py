a0dbb53d4d8df2a4a22a7164a30f4af56a8d14b2
Ananthu-Ravindran <ananthuananthapuri1@gmail.com>
2024-04-25 04:52:51 -0500

    small corrections in the description

--- xedocs/schemas/corrections/implementations/avg_se_gain.py
+++ xedocs/schemas/corrections/implementations/avg_se_gain.py
@@ -1,16 +1,16 @@
 """
-This schema incorporates two piece of inforamtion for the SEG & EE correciont. Namely the average single electron gain
-and the definition of the AB and CD partitions
+This schema incorporates two pieces of information for the SEG & EE correction. Namely the average single electron gain
+and the definition of the AB and CD partitions.
 
 Correction: Average single electron gain
 Affects: Corrected areas
 
-Average of the single electron corrections. This correction my also have two partitions as this average is a temporal average not a spatial one.
+Average of the single electron corrections. This correction may also have two partitions as this average is a temporal average and not a spatial one.
 
 Correction: Region linear and circular for AB/CD partitions
 Affects: Corrected areas
 
-Two distinct patterns of evolution of single electron corrections between A+B and C+D. Distinguish thanks to linear and circular regions
+Two distinct patterns of evolution of single electron corrections between A+B and C+D. Distinguished thanks to linear and circular regions.
 SR0 wiki: https://xe1t-wiki.lngs.infn.it/doku.php?id=jlong:sr0_2_region_se_correction
 SR1 wiki: https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenonnt:noahhood:corrections:se_gain_ee_final
 
