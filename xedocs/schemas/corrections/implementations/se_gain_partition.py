eebb4e62b579ea94fbf7fc0f071ef5a7c80a8a65
Giovanni Volta <38431109+GiovanniVolta@users.noreply.github.com>
2024-04-26 16:25:41 +0200

    Update se_gain_partition.py

--- xedocs/schemas/corrections/implementations/se_gain_partition.py
+++ xedocs/schemas/corrections/implementations/se_gain_partition.py
@@ -2,7 +2,8 @@
 Correction: Region linear and circular for AB/CD partitions
 Affects: Corrected areas
 
-Two distinct patterns of evolution of single electron corrections between A+B and C+D. Distinguish thanks to linear and circular regions
+Two distinct patterns of evolution of single electron corrections between A+B and C+D. Distinguish thanks to linear and circular regions.
+This TPC partitioning affects the single electron gains - both the average value and not - and the relative extraction efficiency.
 
 SR0 wiki: https://xe1t-wiki.lngs.infn.it/doku.php?id=jlong:sr0_2_region_se_correction
 SR1 wiki: https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenonnt:noahhood:corrections:se_gain_ee_final
