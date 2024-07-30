22b9a12814559885139dd50c53501e5e9589fb06
Luis Sanchez <45677170+LuisSanchez25@users.noreply.github.com>
2024-07-30 09:03:45 -0500

    Update test_correction.py

--- xedocs/schemas/corrections/implementations/test_correction.py
+++ xedocs/schemas/corrections/implementations/test_correction.py
@@ -9,11 +9,8 @@ from ...constants import DETECTOR
 class TestCorrection(TimeSampledCorrection):
     _ALIAS = "test_corrections"
 
-    # Here we use a simple indexer (matches on exact value)
-    # to define the pmt field
-    # this will add the field to all documents and enable
-    # selections on the pmt number. Since this is a index field
-    # versioning will be indepentent for each pmt
+    # Correction identical to PMT gains.
+    # Used for testing
 
     detector: DETECTOR = rframe.Index()
     pmt: int = rframe.Index(ge=0)
