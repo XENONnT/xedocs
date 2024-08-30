3d7f8576a933e2e6b4b61483332d1d0d7a4648e8
Ananthu-Ravindran <ananthuananthapuri1@gmail.com>
2024-08-30 04:55:05 -0500

    Detector Operations Log

--- xedocs/schemas/operations_reports/__init__.py
+++ xedocs/schemas/operations_reports/__init__.py
@@ -2,3 +2,4 @@ from .hotspot import HotspotReport
 from .anode_ramp import AnodeRampReport
 from .anode_washing import AnodeWashingReport
 from .abnormal_rates import AbnormalDAQRate
+from .detector_operations import DetectorOperations
